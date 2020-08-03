fe9ac7622675d61b61b158128defaa558f5dd539
Yuriy1302 <ascendo@list.ru>
2020-08-03 20:21:43 +0700

    Debounce

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { Layout, List, Spin, Alert } from 'antd';
+import { Layout, List, Spin, Alert, Input } from 'antd';
+import debounce from 'lodash/debounce';
 import PropTypes from 'prop-types';
 
 import CardMovie from '../CardMovie';
@@ -25,12 +26,14 @@ class App extends Component {
     super(props);
     const { defaultCurrent } = this.props;
     this.state = {
+      searchText: '',
       searchMovie: `https://api.themoviedb.org/3/search/movie?api_key=05f7db0eb20b02a8803d7f7d0f3fb520&language=en-US&query=return&page=${defaultCurrent}&include_adult=false`,
       moviesList: [],
       genreNames: [],
       error: false,
       loading: true,
     };
+    this.debouncedUpdate = debounce(value => this.handleChange(value), 1500);
   }
 
   componentDidMount() {
@@ -64,12 +67,19 @@ class App extends Component {
     this.movieService.getSearchMovies(movie).then(this.onSearchMoviesListLoaded).catch(this.onError);
   };
 
+  handleChange = (value) => {
+    this.setState({ searchText: value });    
+  };
+
   render() {
+    console.log('searchText => ', this.state.searchText)
     const { Content } = Layout;
+    //const { Search } = Input;
 
     const { error, loading, moviesList, genreNames } = this.state;
 
     if (error) {
+      console.log('Что-то пошло не так');
       return (
         <div className="alert_example">
           <Alert
@@ -93,6 +103,11 @@ class App extends Component {
 
     return (
       <Layout className="container">
+        <Input
+          placeholder="Type to search..."
+          onChange={({ target: {value} }) => this.debouncedUpdate(value)}
+          style={{ width: '100%', margin: '20px 0'}}
+        />
         <Content>
           <List
             className="list-align"
